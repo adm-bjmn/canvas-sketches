fb64ad2bafe9f2cfea4c5d8e1126113fa83918ea
Adm.bjmn <adm.bjmn@gmail.com>
2023-09-25 20:31:14 +0100

    drafting: quadrants set up

--- sketch-files/circles-draft.js
+++ sketch-files/circles-draft.js
@@ -9,44 +9,71 @@ const sketch = () => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    let centerX = 0;
-    let centerY = 0;
+    let centerX = 0; // Center X-coordinate
+    let centerY = 0; // Center Y-coordinate
     const radius = 100;
 
     // Quadrants
-    const bottomRight = {
-      startAngle: 0,
-      endAngle: Math.PI / 2,
+    const bottomRight = (context, centerX, centerY, radius) => {
+      const startAngle = 0;
+      const endAngle = Math.PI / 2;
+      // Draw shape
+      context.fillStyle = "pink";
+      context.beginPath();
+      context.moveTo(centerX, centerY); // Move to the start point
+      context.arc(centerX, centerY, radius, startAngle, endAngle); // draw the arc
+      context.lineTo(centerX, centerY); // connect back to the center point to close the shape
+      context.fill();
     };
 
-    const bottomLeft = {
-      startAngle: Math.PI / 2,
-      endAngle: Math.PI,
+    const bottomLeft = (context, centerX, centerY, radius) => {
+      const startAngle = Math.PI / 2;
+      const endAngle = Math.PI;
+      // Move point to the top right Corner
+      context.translate(100, 0);
+      // Draw shape
+      context.fillStyle = "pink";
+      context.beginPath();
+      context.moveTo(centerX, centerY); // Move to the start point
+      context.arc(centerX, centerY, radius, startAngle, endAngle); // draw the arc
+      context.lineTo(centerX, centerY); // connect back to the center point to close the shape
+      context.fill();
     };
 
-    const topRight = {
-      startAngle: Math.PI * 1.5,
-      endAngle: 0,
+    const topLeft = (context, centerX, centerY, radius) => {
+      const startAngle = Math.PI;
+      const endAngle = Math.PI * 1.5;
+      // Move point to the bottom right Corner
+      context.translate(100, 100);
+      // Draw shape
+      context.fillStyle = "pink";
+      context.beginPath();
+      context.moveTo(centerX, centerY); // Move to the start point
+      context.arc(centerX, centerY, radius, startAngle, endAngle); // draw the arc
+      context.lineTo(centerX, centerY); // connect back to the center point to close the shape
+      context.fill();
     };
 
-    const topLeft = {
-      startAngle: Math.PI,
-      endAngle: Math.PI * 1.5,
+    const topRight = (context, centerX, centerY, radius) => {
+      const startAngle = Math.PI * 1.5;
+      const endAngle = 0;
+      // Move point to the bottom left Corner
+      context.translate(0, 100);
+      // Draw shape
+      context.fillStyle = "pink";
+      context.beginPath();
+      context.moveTo(centerX, centerY); // Move to the start point
+      context.arc(centerX, centerY, radius, startAngle, endAngle); // draw the arc
+      context.lineTo(centerX, centerY); // connect back to the center point to close the shape
+      context.fill();
     };
 
     // Draw A Peice'a'tha Pizza
-    context.fillStyle = "pink";
-    context.beginPath();
-    context.moveTo(centerX, centerY); // Move to the start point
-    context.arc(
-      centerX,
-      centerY,
-      radius,
-      bottomRight.startAngle,
-      bottomRight.endAngle
-    ); // draw the arc
-    context.lineTo(centerX, centerY); // connect back to the center point to close the shape
-    context.fill();
+
+    // topLeft(context, centerX, centerY, radius);
+    // topRight(context, centerX, centerY, radius);
+    // bottomRight(context, centerX, centerY, radius);
+    // bottomLeft(context, centerX, centerY, radius);
   };
 };
 
