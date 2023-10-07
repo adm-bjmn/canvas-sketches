96edbc2b4683499af87abf08e61c0fc94f75af33
Adm.bjmn <adm.bjmn@gmail.com>
2023-10-07 21:46:34 +0100

    develop(SB02): random grey length

--- sketch-files/SB02.js
+++ sketch-files/SB02.js
@@ -22,6 +22,7 @@ const sketch = () => {
     let colourSelector = 0;
     let ticker = 0;
     let plotCount = 0;
+    let plotLimit;
 
     let centerY = 0;
     let centerX = 0;
@@ -41,7 +42,7 @@ const sketch = () => {
       context.arc(centerX, centerY, radius, 0, Math.PI * 2);
       context.fill();
     };
-
+    plotLimit = random.rangeFloor(1020, 1045);
     for (let j = height; j > 0; j--) {
       context.save();
       context.translate(centerX + j * 10, height - j * 10);
@@ -49,10 +50,10 @@ const sketch = () => {
       context.restore();
       ticker++;
       plotCount++;
-      if (plotCount < 1020) {
+      if (plotCount < plotLimit) {
         colourSelector = 0;
       } else {
-        if (ticker > 10 && random.value() > 0.5) {
+        if (ticker > 15 && random.value() > 0.8) {
           const randomColour = random.rangeFloor(1, fillColour.length);
           colourSelector = randomColour;
           ticker = 0;
@@ -74,7 +75,7 @@ const sketch = () => {
     //     if (plotCount < 1010) {
     //       colourSelector = 0;
     //     } else {
-    //       if (ticker > 14) {
+    //       if (ticker > 20) {
     //         const randomColour = random.rangeFloor(1, fillColour.length);
     //         colourSelector = randomColour;
     //         ticker = 0;
