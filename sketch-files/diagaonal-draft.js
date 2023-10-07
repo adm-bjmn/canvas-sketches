a33487ee61d910bfbf53415e58cadb6bb5e269ee
Adm.bjmn <adm.bjmn@gmail.com>
2023-10-07 14:34:42 +0100

    develop(diagonals-draft): add colors from brief

--- sketch-files/diagaonal-draft.js
+++ sketch-files/diagaonal-draft.js
@@ -12,10 +12,18 @@ const sketch = () => {
     let centerX = width;
     let centerY = 0;
     let radius = 100;
-    const fillColour = ["black", "blue"];
+    const fillColour = [
+      "#25262C",
+      "#17AD7C",
+      "#37A2B7",
+      "#DB5094",
+      "#08407F",
+      "#FCD5A4",
+      "#F6A12B",
+    ];
 
     const drawCircle = (centerX, centerY, radius) => {
-      context.fillStyle = fillColour[0];
+      context.fillStyle = fillColour[6];
       context.beginPath();
       context.arc(centerX, centerY, radius, 0, Math.PI * 2);
       context.fill();
