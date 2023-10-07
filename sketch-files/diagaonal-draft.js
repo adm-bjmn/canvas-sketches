b94941afa641f4cb72daddae4c475ded88de88a0
Adm.bjmn <adm.bjmn@gmail.com>
2023-10-07 14:21:33 +0100

    new: starbots draft (diagonal-draft.js)

---
+++ sketch-files/diagaonal-draft.js
@@ -0,0 +1,32 @@
+const canvasSketch = require("canvas-sketch");
+
+const settings = {
+  dimensions: [2048, 2048],
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, width, height);
+
+    let centerX = width;
+    let centerY = 0;
+    let radius = 100;
+    const fillColour = ["black", "blue"];
+
+    const drawCircle = (centerX, centerY, radius) => {
+      context.fillStyle = fillColour[0];
+      context.beginPath();
+      context.arc(centerX, centerY, radius, 0, Math.PI * 2);
+      context.fill();
+    };
+
+    for (let i = width; i > 0; i--) {
+      drawCircle(centerX, centerY, radius);
+      centerX -= 10;
+      centerY += 10;
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
