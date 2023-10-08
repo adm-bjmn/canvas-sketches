d89c35ad61be78b36cac46c4939b6ed619e3043c
Adm.bjmn <adm.bjmn@gmail.com>
2023-10-08 16:08:36 +0100

    develop: remove masses of un needed circles

--- sketch-files/SB02.js
+++ sketch-files/SB02.js
@@ -26,7 +26,7 @@ const sketch = () => {
     let plotCount = 0;
     let plotLimit;
     let changed = 0;
-    let centerY = 0;
+    let centerY = height;
     let centerX = 0;
     const fillColour = [
       "#25262C",
@@ -45,58 +45,60 @@ const sketch = () => {
       context.fill();
     };
 
+    // +++++++ TESTING ZONE ++++++
+    // context.translate(centerX, centerY);
+    // drawCircle(0, 0, radius, colourSelector);
 
-    plotLimit = random.rangeFloor(width*0.6, width*0.8);
-
+    plotLimit = random.rangeFloor(width * 0.6, width * 0.8);
     for (let j = height; j > 0; j--) {
       context.save();
-      context.translate(centerX + j * 10, height - j * 10);
+      console.log(centerX + j * 10, centerY - j * 10);
+      context.translate(centerX + j, centerY - j);
       drawCircle(0, 0, radius, colourSelector);
       context.restore();
       ticker++;
       plotCount++;
-      if (centerX + j * 10 > plotLimit) {
+      if (centerX + j > plotLimit) {
         colourSelector = 0;
       } else {
         if (changed < 3) {
-          if (ticker > 15 && random.value() > 0.8) {
-            console.log(changed);
+          if (ticker > 200 && random.value() > 0.8) {
             const randomColour = random.rangeFloor(1, fillColour.length);
             colourSelector = randomColour;
             ticker = 0;
             changed++;
-            console.log(changed);
           }
         }
       }
     }
-
-    // for (let i = -height; i < height; i += radius) {
-    //   plotCount = 0;
-    //   context.save();
-    //   context.translate(0, centerY);
-    //   for (let j = width; j > 0; j--) {
-    //     context.save();
-    //     context.translate(centerX - j * 10, centerY + j * 10);
-    //     drawCircle(0, 0, radius, colourSelector);
-    //     context.restore();
-    //     ticker++;
-    //     plotCount++;
-    //     if (plotCount < 1010) {
-    //       colourSelector = 0;
-    //     } else {
-    //       if (ticker > 20) {
-    //         const randomColour = random.rangeFloor(1, fillColour.length);
-    //         colourSelector = randomColour;
-    //         ticker = 0;
-    //       }
-    //     }
-    //   }
-    //   centerY += radius * 1.4;
-    //   context.restore();
-    // }
-    //context.restore();
   };
+
+  // for (let i = -height; i < height; i += radius) {
+  //   plotCount = 0;
+  //   context.save();
+  //   context.translate(0, centerY);
+  //   for (let j = width; j > 0; j--) {
+  //     context.save();
+  //     context.translate(centerX - j * 10, centerY + j * 10);
+  //     drawCircle(0, 0, radius, colourSelector);
+  //     context.restore();
+  //     ticker++;
+  //     plotCount++;
+  //     if (plotCount < 1010) {
+  //       colourSelector = 0;
+  //     } else {
+  //       if (ticker > 20) {
+  //         const randomColour = random.rangeFloor(1, fillColour.length);
+  //         colourSelector = randomColour;
+  //         ticker = 0;
+  //       }
+  //     }
+  //   }
+  //   centerY += radius * 1.4;
+  //   context.restore();
+  // }
+  //context.restore();
+  //};
 };
 
 canvasSketch(sketch, settings);
